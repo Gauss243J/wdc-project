dacb64865edb7bdcf5ba0c59293eec60f32074c3
Gauss243J <78795548+Gauss243J@users.noreply.github.com>
2024-07-24 20:10:18 +0200

    Update example.py
    remove visibility

--- example.py
+++ example.py
@@ -4,7 +4,18 @@ import toml
 from streamlit_chat import message
 
 # Set page title and header
-st.set_page_config(page_title="WDC", page_icon=":globe_with_meridians:")
+st.set_page_config(page_title="WDC", page_icon=":globe_with_meridians:", layout="centered", initial_sidebar_state="auto)
+# Disable the default Streamlit menu
+hide_streamlit_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    </style>
+    """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
 st.markdown("<h1 style='text-align: center;'>World Disaster Center 🌐</h1>", unsafe_allow_html=True)
 
 # Load API key from environment variable
